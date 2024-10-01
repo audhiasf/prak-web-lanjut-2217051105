8908a795dc71c179702bce41319cdf1794a3a87d
audhiasf <audysafitri060@gmail.com>
2024-10-01 16:40:41 +0700

    perubahan 4

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -34,15 +34,29 @@
                 @csrf
                 <div class="mb-3">
                     <label for="nama" class="form-label">Nama:</label>
-                    <input type="text" id="nama" name="nama" class="form-control" placeholder="Masukkan Nama">   
+                    <input type="text" id="nama" name="nama" class="form-control" placeholder="Masukkan Nama">
+                    @foreach($errors->get('nama') as $msg)
+                        <p class="text-danger">Nama Harus Diisi</p>
+                    @endforeach     
                 </div>
                 <div class="mb-3">
                     <label for="npm" class="form-label">NPM:</label>
                     <input type="text" id="npm" name="npm" class="form-control" placeholder="Masukkan NPM">
+                    @foreach($errors->get('npm') as $msg)
+                        <p class="text-danger">NPM Harus Diisi</p>
+                    @endforeach 
                 </div>
                 <div class="mb-3">
                     <label for="kelas" class="form-label">Kelas:</label>
-                    <input type="text" id="kelas" name="kelas" class="form-control" placeholder="Masukkan Kelas">
+                    <select name="kelas_id" id="kelas_id" class="form-select" >
+                        <option value="" disabled selected>Pilih Kelas</option>
+                        @foreach($kelas as $kelasItem)
+                        <option value="{{$kelasItem->id}}">{{$kelasItem->nama_kelas}}</option>
+                        @endforeach
+                    </select>
+                    @foreach($errors->get('kelas_id') as $msg)
+                        <p class="text-danger">Kelas Harus Dipilih</p>
+                    @endforeach                 
                 </div>
                 <div class="text-center">
                     <input type="submit" class="btn btn-custom" value="Submit">
